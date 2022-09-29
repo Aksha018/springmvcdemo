145c48ba95581a4e2f834e9aed167f6b05be8b2a
Aksha018 <105410263+Aksha018@users.noreply.github.com>
2022-09-29 16:51:46 +0530

    Swagger Updated

--- moviebooking/src/main/java/com/chainsys/moviebooking/MoviebookingApplication.java
+++ moviebooking/src/main/java/com/chainsys/moviebooking/MoviebookingApplication.java
@@ -3,7 +3,10 @@ package com.chainsys.moviebooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MoviebookingApplication {
 
 	public static void main(String[] args) {
